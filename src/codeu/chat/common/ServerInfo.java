7719eeb31d89bd13dd50ef3c5fa100df53759866
ComarPers922 <18982127626@163.com>
2017-05-28 22:49:59 +0800

    ServerInfo Modified

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -7,16 +7,19 @@ public final class ServerInfo {
   private final static String SERVER_VERSION = "1.0.0";
 
   public final Uuid version;
-  public ServerInfo() throws IOException {
+  public ServerInfo(){
       Uuid temp;
       try {
         temp = Uuid.parse(SERVER_VERSION);
+        this.version = temp;
       } catch (IOException e) {
-          String line1 = "There is a fatal error when I parse version data, please contact Yuhang Liao to fix that!";
-          String line2 = "Also don't forget tell the careless Yuhang Liao this exception happed in Line 15 in ServerInfo.java'";
-          throw new IOException(line1 + "\n" + line2, e);
+          System.out.println("There is a fatal error when I parse version data, please contact Yuhang Liao to fix that!");
+
+          System.out.println("Also don't forget tell the careless Yuhang Liao this exception happed in Line 15 in ServerInfo.java'");
+
+          System.out.println("All server info will be null this time.");
+          this.version = null;
       }
-      this.version = temp;
   }
 
   public ServerInfo(Uuid version) {
