7e0363a7e20310ea55f8ee320ac7645b3b5628f3
ComarPers922 <18982127626@163.com>
2017-06-22 18:32:32 +0800

    Fix some bugs.

--- src/codeu/chat/common/Message.java
+++ src/codeu/chat/common/Message.java
@@ -17,7 +17,6 @@ package codeu.chat.common;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Comparator;
 import java.util.UUID;
 
 import codeu.chat.util.Serializer;
