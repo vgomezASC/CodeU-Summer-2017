d7e4d68c941bed3a688a4095fe181e235a0e08e8
lepidopterane-atsmith <sabowitz@smith.edu>
2017-06-29 12:54:21 -0400

    Updated ServerInfo
    Should be clear to work now!

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -1,12 +1,18 @@
 package codeu.chat.common;
 
+
 import java.io.IOException;
+import codeu.chat.util.Time;
+
 import codeu.chat.util.Uuid;
 public final class ServerInfo {
   private final static String SERVER_VERSION = "1.0.0";
+    
+  public final Time startTime;
   public final Uuid version;
 
   public ServerInfo(){
+      this.startTime = Time.now();
       Uuid temp = null;
       try {
         temp = Uuid.parse(SERVER_VERSION);
@@ -20,7 +26,14 @@ public final class ServerInfo {
       }
   }
 
-  public ServerInfo(Uuid version) {
+  public ServerInfo(Uuid version, Time startTime) {
     this.version = version;
+    this.startTime = startTime;
+  }
+  
+  public String upTime() {
+    long endTime = System.currentTimeMillis();
+    long totalTime = endTime - startTime.inMs();
+    return ""+totalTime + " ms"; 
   }
 }
